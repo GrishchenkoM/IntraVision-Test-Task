d88c848f577ad9e5bd58c770328b0a6742dac8a5
Michael <grishchenko.m@yandex.ru>
2016-10-30 13:33:38 +0200

    Cretae GetDrinksBySum and MakePurchase methods

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,18 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using BusinessLogic;
+using Web.Models;
+using Web.Models.EnityModels;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(DataManager dataManager)
+        {
+            _modelFactory = new PurchaseModelFactory(dataManager);
+        }
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
         }
+
+        public JsonResult GetDrinksBySum(List<CoinModel> coinModel)
+        {
+            try
+            {
+                var model = _modelFactory.Create(coinModel);
+
+                return Json(model, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json("Inrternal Server Error: " + ex.Message);
+            }
+        }
+        public JsonResult MakePurchase([FromBody]List<CoinModel> coinModel, int id)
+        {
+            var model = _modelFactory.Create(coinModel, id);
+
+            return Json(model);
+        }
+
+        private readonly PurchaseModelFactory _modelFactory;
     }
 }
