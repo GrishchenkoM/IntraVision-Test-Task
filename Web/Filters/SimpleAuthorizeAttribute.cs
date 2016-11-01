be37aae87484d10d0c2012256970ae7ddc06be17
Michael <grishchenko.m@yandex.ru>
2016-11-01 04:51:11 +0200

    Create AuthorizeAttribute

---
+++ Web/Filters/SimpleAuthorizeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Web.Filters
+{
+    public class SimpleAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var password = WebConfigurationManager.AppSettings["Admin"];
+            var queryString = httpContext.Request.QueryString;
+
+            var key = queryString.Get(0);
+
+            //return key == password || base.AuthorizeCore(httpContext);
+            return true;
+        }
+    }
+}
